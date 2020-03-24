4a3d3ec3686b573d7c97a1f3359782f1492c4b75
Dash <dascastrillo@gmail.com>
2020-03-24 21:47:26 +0100

    ejercicio7while.py

--- Python/Pruebas/Ejercicios/ejercicio7while.py
+++ Python/Pruebas/Ejercicios/ejercicio7while.py
@@ -1,19 +1,25 @@
 import random
+intentosRealizados = 0
+print('¡Hola! ¿Cómo te llamas?')
+miNombre = input()
+número = random.randint(1, 20)
+print('Bueno, ' + miNombre + ', estoy pensando en un número entre 1 y 20.')
 
-print('Adivina el Numero')
-maquina=random.randint(1, 20)
-usuario= int(input('Introduce un numero'))
-while maquina < 6:
-    maquina += 1
-    suerte=random.randint(1,20)
+while intentosRealizados < 6:
 
-    if suerte < usuario:
-        print('Tu suerte es muy alta')
-    if suerte > usuario:
-        print('Tu suerte es muy baja')
-    if suerte == usuario:
-        break
-if maquina == usuario:
-    print('Has adivinado el numero.')
-else:
-    print('No has adivinado el numero.')
+    print('Intenta adivinar.') # Hay cuatro espacios delante de print.
+    estimación = input()
+    estimación = int(estimación)
+    intentosRealizados = intentosRealizados + 1
+if estimación < número:
+    print('Tu estimación es muy baja.') # Hay ocho espacios delante de print.
+    if estimación > número:
+        print('Tu estimación es muy alta.')
+    if estimación == número:
+        exit
+if estimación == número:
+    intentosRealizados = str(intentosRealizados)
+    print('¡Buen trabajo, ' + miNombre + '! ¡Has adivinado mi número en ' + intentosRealizados + ' intentos!')
+if estimación != número:
+    número = str(número)
+    print('Pues no. El número que estaba pensando era ' + número)
